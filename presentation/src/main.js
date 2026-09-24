2db59de9df2e25640cd3397c323accc3664e0797
Vasco Schwarze <vasco.schwarze@sciota.io>
2026-09-24 23:20:04 +0200

    Final slides improvements

--- presentation/src/main.js
+++ presentation/src/main.js
@@ -1,6 +1,7 @@
 import Reveal from "reveal.js";
 import "reveal.js/reveal.css";
 import "reveal.js/theme/black.css";
+// import "reveal.js/theme/beige.css"; // uninvert sciota-logo, astro logo, islands architecture diagram and metrics tables if used
 import "../assets/atom-one-dark.min.css";
 
 import RevealHighlight from "reveal.js/plugin/highlight";
